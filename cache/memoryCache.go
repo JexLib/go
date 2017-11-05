3978ff59ee171846ccf58b161f6f26a564d74c7d
Justin Huang <Justin Huang>
2017-11-05 23:23:12 +0800

    add

---
+++ cache/memoryCache.go
@@ -0,0 +1,111 @@
+package cache
+
+import (
+	"container/list"
+	"fmt"
+	"sync"
+	"time"
+)
+
+type MemoryCache struct {
+	defaultExpiration time.Duration
+	items             *list.List
+	mu                *sync.Mutex
+}
+
+func NewMemoryCache(defaultExpiration time.Duration, cleanupInterval ...time.Duration) *MemoryCache {
+	c := &MemoryCache{
+		defaultExpiration: defaultExpiration,
+		items:             list.New(),
+		mu:                new(sync.Mutex),
+	}
+
+	defCleanupInterval := time.Second * 5
+	if len(cleanupInterval) > 0 && cleanupInterval[0] > 0 {
+		defCleanupInterval = cleanupInterval[0]
+	}
+	c.gc(defCleanupInterval)
+
+	return c
+}
+
+func (mc *MemoryCache) Set(key string, value interface{}, expiration ...time.Duration) error {
+	d := mc.defaultExpiration
+	if len(expiration) > 0 {
+		d = expiration[0]
+	}
+	item := new_CacheItem(key, value, d)
+
+	defer mc.mu.Unlock()
+	mc.mu.Lock()
+	if e := mc.find(key); e != nil {
+		e.Value = *item
+	} else {
+		mc.items.PushBack(*item)
+	}
+	fmt.Println("set:", *item)
+	return nil
+}
+
+func (mc *MemoryCache) Get(key string) interface{} {
+	if e := mc.find(key); e != nil {
+		return e.Value.(_CacheItem).Val
+	}
+	return nil
+}
+
+func (mc *MemoryCache) Delete(key string) error {
+	if e := mc.find(key); e != nil {
+		defer mc.mu.Unlock()
+		mc.mu.Lock()
+		mc.items.Remove(e)
+	}
+	return nil
+}
+
+func (mc *MemoryCache) find(key string) *list.Element {
+	for e := mc.items.Front(); e != nil; e = e.Next() {
+		if e.Value.(_CacheItem).Key == key && !e.Value.(_CacheItem).Expired() {
+			return e
+		}
+	}
+	return nil
+}
+
+func (mc *MemoryCache) Count() int {
+	return mc.items.Len()
+}
+
+//清空数据
+func (mc *MemoryCache) Clear() error {
+	defer mc.mu.Unlock()
+	mc.mu.Lock()
+	mc.items.Init()
+	return nil
+}
+
+func (mc *MemoryCache) LoadFromFile(filename string) error {
+	return nil
+}
+
+func (mc *MemoryCache) SaveToFile(filename string) error {
+
+	// var d1 = []byte(wireteString);
+	// err2 := ioutil.WriteFile("./output2.txt", d1, 0666)  //写入文件(字节数组)
+	return nil
+}
+
+func (mc *MemoryCache) gc(gcInterval time.Duration) {
+	go func() {
+		for {
+			for e := mc.items.Front(); e != nil; e = e.Next() {
+				if e.Value.(_CacheItem).Expired() {
+					mc.mu.Lock()
+					mc.items.Remove(e)
+					mc.mu.Unlock()
+				}
+			}
+			time.Sleep(gcInterval)
+		}
+	}()
+}
